2ddfec757b0e00dd2c6e66283d3753b7f270589a
Alejandro <42224066+altamayo02@users.noreply.github.com>
2024-04-11 23:32:06 -0500

    Laid out boilerplates for models and controllers

--- app/Controllers/Http/ReviewsController.ts
+++ app/Controllers/Http/ReviewsController.ts
@@ -1,3 +1,39 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Review from 'App/Models/Review'
 
-export default class ReviewsController {}
+export default class ReviewsController {
+	public async find({ request, params }: HttpContextContract) {
+        if (params.id) {
+            return await Review.findOrFail(params.id)
+        } else {
+            const data = request.all()
+            if ('page' in data && 'per_page' in data) {
+                const page = request.input('page', 1)
+                const perPage = request.input('per_page', 20)
+                return await Review.query().paginate(page, perPage)
+            } else {
+                return await Review.query()
+            }
+        }
+    }
+
+    public async create({ request }: HttpContextContract) {
+        const body = request.body()
+        const theReview: Review = await Review.create(body)
+        return theReview
+    }
+
+    public async update({ params, request }: HttpContextContract) {
+        const theReview: Review = await Review.findOrFail(params.id)
+        const body = request.body()
+		// TODO - Assign attributes in body to model
+		// theReview.attr = body.attr
+        return await theReview.save()
+    }
+
+    public async delete({ params, response }: HttpContextContract) {
+        const theReview: Review = await Review.findOrFail(params.id)
+        response.status(204)
+        return await theReview.delete()
+    }
+}
--- app/Controllers/Http/ServicesController.ts
+++ app/Controllers/Http/ServicesController.ts
@@ -1,39 +1,39 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Message from 'App/Models/Message'
+import Service from 'App/Models/Service'
 
-export default class MessagesController {
+export default class ServicesController {
 	public async find({ request, params }: HttpContextContract) {
         if (params.id) {
-            return await Message.findOrFail(params.id)
+            return await Service.findOrFail(params.id)
         } else {
             const data = request.all()
             if ('page' in data && 'per_page' in data) {
                 const page = request.input('page', 1)
                 const perPage = request.input('per_page', 20)
-                return await Message.query().paginate(page, perPage)
+                return await Service.query().paginate(page, perPage)
             } else {
-                return await Message.query()
+                return await Service.query()
             }
         }
     }
 
     public async create({ request }: HttpContextContract) {
         const body = request.body()
-        const theMessage: Message = await Message.create(body)
-        return theMessage
+        const theService: Service = await Service.create(body)
+        return theService
     }
 
     public async update({ params, request }: HttpContextContract) {
-        const theMessage: Message = await Message.findOrFail(params.id)
+        const theService: Service = await Service.findOrFail(params.id)
         const body = request.body()
 		// TODO - Assign attributes in body to model
-		// theMessage.attr = body.attr
-        return await theMessage.save()
+		// theService.attr = body.attr
+        return await theService.save()
     }
 
     public async delete({ params, response }: HttpContextContract) {
-        const theMessage: Message = await Message.findOrFail(params.id)
+        const theService: Service = await Service.findOrFail(params.id)
         response.status(204)
-        return await theMessage.delete()
+        return await theService.delete()
     }
 }
