2050992b2520268e60d42119db7b47966346f950
pailas02 <jesus.botero22412@ucaldas.edu.co>
2024-05-08 19:23:13 -0500

    subitida

--- database/migrations/1712889820714_services.ts
+++ database/migrations/1712889820714_services.ts
@@ -6,13 +6,18 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('name') // name of the service
+      table.string('description') // description of the service
+      table.boolean('status') // status of the service
+      table.double('cost')//cost of the service
+
+
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
-      String.description('description')
       table.integer('cost')
     })
   }
