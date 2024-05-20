d67a28eb2ea46b9de847839a3643fd6ebec075ca
altamayo02 <korkonica@gmail.com>
2024-05-20 13:52:56 -0500

    Deleted duplicate classes from exam

--- start/routes/camara.ts
+++
@@ -1,6 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-Route.group(() => {
-	Route.get('/camaras', 'CamarasController.find')
-	Route.get('/camaras/:id', 'CamarasController.find')
-	Route.post('/camaras', 'CamarasController.create')
-}).middleware(['security'])
--- start/routes/stream.ts
+++
@@ -1,6 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-Route.group(() => {
-	Route.get('/streams', 'StreamsController.find')
-	Route.get('/streams/:id', 'StreamsController.find')
-	Route.post('/streams', 'StreamsController.create')
-}).middleware(['security'])
